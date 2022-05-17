84df6829a680d88231f6eb3457be14b176dff103
IT20146238 <it20146238@my.sliit.lk>
2022-05-18 03:37:08 +0530

    Add All CRUD Operation API routes

--- UserManagement/backend/routes/UserRoute.js
+++ UserManagement/backend/routes/UserRoute.js
@@ -1,10 +1,19 @@
 import express from "express";
 
-import { getUsers } from "../controllers/UserController.js";
+import {
+  getUsers,
+  getUserById,
+  saveUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/UserController.js";
 
 const router = express.Router();
 
-router.get('/users', getUsers);
-
+router.get("/users", getUsers);
+router.get("/users/:id", getUserById);
+router.post("/users", saveUser);
+router.patch("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
 
 export default router;
