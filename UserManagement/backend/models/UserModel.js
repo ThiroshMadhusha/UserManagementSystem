a3e8878b94b2541f7fe217e7c521531df290d1ce
IT20146238 <it20146238@my.sliit.lk>
2022-05-18 01:57:32 +0530

    create user management model class

---
+++ UserManagement/backend/models/UserModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const User = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+        
+    },
+    email: {
+        type: String,
+        required: true
+        
+    },
+    gender: {
+        type: String,
+        required: true
+
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+
+    
+});
+export default mongoose.model('Users', User);
