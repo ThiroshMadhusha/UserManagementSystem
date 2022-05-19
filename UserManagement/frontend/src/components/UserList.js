f6ff0aa73feb2764791baa455c828fd44a4151a5
IT20146238 <it20146238@my.sliit.lk>
2022-05-20 03:28:21 +0530

    Create View Table in User List

---
+++ UserManagement/frontend/src/components/UserList.js
@@ -0,0 +1,54 @@
+// Short cut key --> rafce
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const UserList = () => {
+  const [users, setUser] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    const response = await axios.get("http://localhost:5000/users");
+    setUser(response.data);
+  };
+  return (
+    <div className="columns">
+      <div className="column">
+        <table className="table is-striped is-fullwidth mt-5">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Gender</th>
+              <th>Address</th>
+              <th>Phone</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <td>{index + 1}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.gender}</td>
+                <td>{user.address}</td>
+                <td>{user.phone}</td>
+                <td>
+                  <button className="button is-warning is-small">Update</button>
+                  <button className="button is-success is-small">Update</button>
+                  <button className="button is-danger is-small">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
